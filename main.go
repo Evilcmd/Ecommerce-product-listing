f6c7f55fd5789e0c8fa54fac84cfad789f876649
Evilcmd <tejonweb28@gmail.com>
2024-07-29 11:09:58 +0530

    spin up a basic server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	log.Println("Hello World")
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	router := http.NewServeMux()
+
+	// Test routes
+	router.HandleFunc("GET /health", checkHealth)
+	router.HandleFunc("GET /error", errCheck)
+
+	// General Routes
+	router.HandleFunc("GET /", rootEndpoint)
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: router,
+	}
+	fmt.Printf("Starting server on port: %v\n", port)
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("Error in starting server: %v\n", err.Error())
+	}
 }
